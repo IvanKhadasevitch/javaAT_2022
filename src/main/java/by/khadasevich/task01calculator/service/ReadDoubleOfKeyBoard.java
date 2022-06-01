1a7676c24ab2f86587901d055e8da4c6bd5a7003
Ivan Khadasevich <adv.club.ivan@gmail.com>
2022-06-01 11:59:44 +0300

    add interface ReadDoubleOfKeyBoard to package by.khadasevich.task01calculator.service

---
+++ src/main/java/by/khadasevich/task01calculator/service/ReadDoubleOfKeyBoard.java
@@ -0,0 +1,10 @@
+package by.khadasevich.task01calculator.service;
+
+public interface ReadDoubleOfKeyBoard {
+    /**
+     * Read double value entered by user.
+     * @param message that user see before entering
+     * @return double value entered by user
+     */
+    double readDouble(String message);
+}
